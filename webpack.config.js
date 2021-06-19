906daa9d82ddc5a9e6daa0b2390d8db5aa43d83e
infinity <infinity@localhost.localdomain>
2021-06-19 14:08:45 -0400

    Added Contact

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "development",
+  mode: "production",
   context: path.resolve(__dirname, "assets/scss"),
   entry: "./style.scss",
   devtool: "source-map",
